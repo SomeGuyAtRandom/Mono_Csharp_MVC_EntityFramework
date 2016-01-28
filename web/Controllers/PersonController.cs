d55685e3c0acda6124d339e276ebc42366a6b5f6
Some Guy <someguy@random.la>
2016-01-28 11:15:49 -0800

    Added DbContext 'PeopleContext' and tested insert

--- web/Controllers/PersonController.cs
+++ web/Controllers/PersonController.cs
@@ -4,13 +4,35 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using web.ContextDbs;
+using web.Models;
+
 namespace web.Controllers
 {
     public class PersonController : Controller
     {
+		private PeopleContext db;
+
+		public PersonController()
+		{
+			this.db = new PeopleContext();
+		}
         public ActionResult Index()
         {
             return View ();
         }
+
+		public ActionResult TestAddtion()
+		{
+			
+			Person person = new Person () {
+				FirstName = "Some",
+				LastName ="Guy"
+			};
+			db.People.Add (person);
+			db.SaveChanges();
+
+			return RedirectToAction("Index","Home"); 
+		} 
     }
 }
